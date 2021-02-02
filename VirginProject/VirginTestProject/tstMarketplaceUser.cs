8d75dacfe4b664ba4e47cd56d0275c922dabc4b2
Callum Simpson <P2444354@my365.dmu.ac.uk>
2021-02-02 12:29:17 +0000

    Added Class file for marketplace users with associated test class.

---
+++ VirginProject/VirginTestProject/tstMarketplaceUser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VirginClassLibrary;
+
+namespace VirginTestProject
+{
+    [TestClass]
+    public class tstMarketplaceUser
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create instance of Marketplace user
+            clsMarketplaceUser AnMarketplaceUser = new clsMarketplaceUser();
+            //test to see if it existis
+            Assert.IsNotNull(AnMarketplaceUser);
+        }
+    }
+}
