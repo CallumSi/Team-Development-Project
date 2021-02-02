0a734a686204835b99c65faaeca5b3876ad9c46b
Callum Simpson <P2444354@my365.dmu.ac.uk>
2021-02-02 15:56:00 +0000

    Added Delivery Attributes to MarketplaceUser class and tested them

--- VirginProject/VirginClassLibrary/clsMarketplaceUser.cs
+++ VirginProject/VirginClassLibrary/clsMarketplaceUser.cs
@@ -3,5 +3,8 @@
     public class clsMarketplaceUser
     {
         public bool Admin { get; set; }
+        public string DeliveryAdressLineOne { get; set; }
+        public string DeliveryAdressLineTwo { get; set; }
+        public string PostCode { get; set; }
     }
 }
--- VirginProject/VirginTestProject/tstMarketplaceUser.cs
+++ VirginProject/VirginTestProject/tstMarketplaceUser.cs
@@ -27,7 +27,62 @@ namespace VirginTestProject
             AnMarketplaceUser.Admin = TestData;
             //test if values are equal 
             Assert.AreEqual(AnMarketplaceUser.Admin, TestData);
+
+
         }
+
+
+      
+
+
+
+        [TestMethod]
+        public void DeliveryAdressLineOnePropertyOK()
+        {
+            //HouseNumber + Street name
+
+            //create instance of Marketplace user
+            clsMarketplaceUser AnMarketplaceUser = new clsMarketplaceUser();
+            //test data for property
+            string TestData = "77 Wall Street";
+            //assign data to the property
+            AnMarketplaceUser.DeliveryAdressLineOne = TestData;
+            //test if values are equal 
+            Assert.AreEqual(AnMarketplaceUser.DeliveryAdressLineOne, TestData);
+        }
+
+        [TestMethod]
+        public void DeliveryAdressLineTwoPropertyOK()
+        {
+            //City
+
+            //create instance of Marketplace user
+            clsMarketplaceUser AnMarketplaceUser = new clsMarketplaceUser();
+            //test data for property
+            string TestData = "New York";
+            //assign data to the property
+            AnMarketplaceUser.DeliveryAdressLineTwo = TestData;
+            //test if values are equal 
+            Assert.AreEqual(AnMarketplaceUser.DeliveryAdressLineTwo, TestData);
+        }
+
+
+        [TestMethod]
+        public void PostCodePropertyOK()
+        {
+            //postcode
+
+            //create instance of Marketplace user
+            clsMarketplaceUser AnMarketplaceUser = new clsMarketplaceUser();
+            //test data for property
+            string TestData = "LE1 5BD";
+            //assign data to the property
+            AnMarketplaceUser.PostCode = TestData;
+            //test if values are equal 
+            Assert.AreEqual(AnMarketplaceUser.PostCode, TestData);
+        }
+
+
 
     }
 }
