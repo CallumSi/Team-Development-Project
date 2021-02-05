85e740be00d315d11b9ab4fe6b4760206f925c96
Mathunila Nitharsan <P17192949@my365.dmu.ac.uk>
2021-02-05 21:54:06 +0000

    Created testing for Patient, Staff, Hospital and Appointment with its collections accordingly.

---
+++ VirginProject/VirginTestProject/tstVHCHospitalCollection.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace VirginTestProject
+{
+    [TestClass]
+    public class tstVHCHospitalCollection
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ VirginProject/VirginTestProject/tstVHCStaff.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace VirginTestProject
+{
+    [TestClass]
+    public class tstVHCStaff
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
