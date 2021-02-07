3a6fbd9daa7e3ab4d164ad9156f37da2de6ade12
Mathunila Nitharsan <P17192949@my365.dmu.ac.uk>
2021-02-07 13:41:29 +0000

    Applied the valid method under tstVHCpatient for Patient_Title!

--- VirginProject/VirginClassLibrary/clsVHCPatient.cs
+++ VirginProject/VirginClassLibrary/clsVHCPatient.cs
@@ -16,22 +16,27 @@ namespace VirginClassLibrary
         //Patient_Address Property 
         public string Patient_Address { get; set; }
 
+        //Patient_DOB Property
+        public DateTime Patient_DOB { get; set; }
+
         //Patient_Email Property 
         public string Patient_Email { get; set; }
-
-        //Patient_Password Property 
-        public string Patient_Password { get; set; }
 
         //Patient_Username Property
         public string Patient_Username { get; set; }
 
+        //Patient_Password Property 
+        public string Patient_Password { get; set; }
+
         //Patient_Telephone Property
         public string Patient_Telephone { get; set; }
 
-        //Patient_DOB Property
-        public DateTime Patient_DOB { get; set; }
-
         //Patient_Status Property
         public bool Patient_Status { get; set; }
+
+        public Boolean Valid(string Patient_Title)
+        {
+            return true;
+        }
     }
 }
