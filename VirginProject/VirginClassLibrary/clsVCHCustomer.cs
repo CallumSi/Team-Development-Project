424b28ec894a314d094b36b34912b6e5c4bafe05
EdwardTafi <59883163+EdwardTafi@users.noreply.github.com>
2021-02-07 19:49:55 +0000

    I have added tests for my customer class and created it within the class libary.

---
+++ VirginProject/VirginClassLibrary/clsVCHCustomer.cs
@@ -0,0 +1,11 @@
+﻿namespace VirginClassLibrary
+{
+    public class clsVCHCustomer
+    {
+        //customer First Name
+        public string FirstName { get; set; }
+
+        //customer Last Name
+        public string LastName { get; set; }
+    }
+}
---
+++ VirginProject/VirginTestProject/tstVCHCustomer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VirginClassLibrary;
+
+namespace VirginTestProject
+{
+    [TestClass]
+    public class tstVCHCustomer
+    {
+        [TestMethod]
+        public void InstantiationOK()
+        {
+            //create an instance of the class
+            clsVCHCustomer VCHCustomer = new clsVCHCustomer();
+        }
+
+        [TestMethod]
+        public void FirstName()
+        {
+            //create an instance of the class
+            clsVCHCustomer VCHCustomer = new clsVCHCustomer();
+            //create a variable to store the customers first name 
+            string SomeFirstName;
+            //assign a FirstName to the variable
+            SomeFirstName = "Dante";
+            //try send data to the FirstName property
+            VCHCustomer.FirstName = SomeFirstName;
+            //check to see that the data in the variable and the property are the same
+            Assert.AreEqual(VCHCustomer.FirstName, SomeFirstName);
+        }
+
+        [TestMethod]
+        public void LastName()
+        {
+            //create an instance of the class
+            clsVCHCustomer VCHCustomer = new clsVCHCustomer();
+            //create a variable to store the customers first name 
+            string SomeLastName;
+            //assign a LastName to the variable
+            SomeLastName = "Alighieri";
+            //try send data to the LastName property
+            VCHCustomer.LastName = SomeLastName;
+            //check to see that the data in the variable and the property are the same
+            Assert.AreEqual(VCHCustomer.LastName, SomeLastName);
+        }
+
+        //leaving work at this stage because my tests aren't running anymore. All I'm getting is a blue exclamation mark
+    }
+}
