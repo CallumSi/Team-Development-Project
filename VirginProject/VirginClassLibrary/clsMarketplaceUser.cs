dcd95443090faee137ee035c2ee03d23edc477ec
Callum Simpson <callumjohnsimpson@hotmail.com>
2021-02-09 16:06:52 +0000

    Updated valid method and completed blank tests

--- VirginProject/VirginClassLibrary/clsMarketplaceUser.cs
+++ VirginProject/VirginClassLibrary/clsMarketplaceUser.cs
@@ -12,29 +12,75 @@ namespace VirginClassLibrary
         public string Password { get; set; }
         public int Rating { get; set; }
 
-        public string Valid(string anDeliveryAdressLineOne)
+        public string Valid(string anDeliveryAdressLineOne, string anDeliveryAdressLineTwo, string anPostCode, string anEmail, string anPassword, string anRating)
         {
-            if (anDeliveryAdressLineOne != "" )
+            //declare empty string 
+            string ErrorMsg = "";
+            int ConvertedRating;
+            //check for blank string
+            if (anDeliveryAdressLineOne.Length>0 && anDeliveryAdressLineOne.Length< 46)
             {
-                if (anDeliveryAdressLineOne.Length < 46)
+                if (anDeliveryAdressLineTwo.Length > 0 && anDeliveryAdressLineTwo.Length < 46)
                 {
-                    //return no error message 
-                    return "";
+                    if (anPostCode.Length>0 && anPostCode.Length<16)
+                    {
+                        if (anEmail.Length>0 && anEmail.Length<50)
+                        {
+                            if (anPassword.Length>0 && anPassword.Length < 50)
+                            {
+                                try
+                                {
+                                    ConvertedRating=Convert.ToInt32(anRating);
+                                    if (ConvertedRating > -1 && ConvertedRating < 11)
+                                    {
+                                        ErrorMsg = "";
+                                    }
+                                    else
+                                    {
+                                        //return error message 
+                                        ErrorMsg += "Rating must be out of ten ";
+                                    }
+                                }
+                                catch{
+                                    //return error message 
+                                    ErrorMsg += "Rating must be int ";
+
+                                }
+                                
+                            }
+                            else
+                            {
+                                //return error message 
+                                ErrorMsg += "Password must be between 0 and 50 characters ";
+                            }
+                        }
+                        else
+                        {
+                            //return error message 
+                            ErrorMsg += "Email must be between 0 and 50 characters ";
+                        }
+
+                    }
+                    else
+                    {
+                        //return error message 
+                        ErrorMsg += "Postcode must be between 0 and 15 characters ";
+                    }
                 }
                 else
                 {
                     //return error message 
-                    return "Delivery Line One cannot exceed 45 characters";
+                    ErrorMsg += "Delivery Line Two must be between 0 and 46 characters ";
+
                 }
-                
             }
             else
             {
                 //return error message 
-                return "Delivery Line One cannot be left blank! ";
+                ErrorMsg += "Delivery Line One must be between 0 and 46 characters";
             }
-        
-        }
 
+            return ErrorMsg;
+        }
     }
 }
