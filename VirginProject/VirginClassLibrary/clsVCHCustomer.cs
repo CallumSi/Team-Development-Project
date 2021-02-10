70c944839c9830bf7dec871bad97e305e1d48d9e
EdwardTafi <59883163+EdwardTafi@users.noreply.github.com>
2021-02-10 15:08:26 +0000

    Added further tests.

--- VirginProject/VirginClassLibrary/clsVCHCustomer.cs
+++ VirginProject/VirginClassLibrary/clsVCHCustomer.cs
@@ -1,4 +1,6 @@
-﻿namespace VirginClassLibrary
+﻿using System;
+
+namespace VirginClassLibrary
 {
     public class clsVCHCustomer
     {
@@ -7,5 +9,39 @@
 
         //customer Last Name
         public string LastName { get; set; }
+
+        //customer Address
+        public string Address { get; set; }
+
+        //customer PhoneNumber
+        public string PhoneNumber { get; set; }
+
+        //customer Email
+        public string Email { get; set; }
+
+        //customer Password
+        public string Password { get; set; }
+
+        public Boolean Valid(string FirstName)
+        {
+            //var to record any errors found in FirstName 
+            Boolean OK = true;
+            //test to see if the FirstName has 1 character
+            if (FirstName.Length == 1)
+            {
+                //set OK to false
+                OK = false;
+            }
+            
+            //test to see that FirstName has no more than 30 character
+            if (FirstName.Length > 30)
+            {
+                //set OK to false
+                OK = false;
+            }
+
+            //return all test results
+            return OK;
+        }
     }
 }
