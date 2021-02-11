3ddfaf7ab9d285655e86cc51ddbae1fb67d3685d
p17210836 <p17210836@GWHXX652WIN014W>
2021-02-11 17:04:51 +0000

    added testing

---
+++ VirginProject/VirginClassLibrary/clsVPCustomer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace VirginClassLibrary
+{
+    public class clsVPCustomer
+    {
+        public string Address { get; set; }
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Postcode { get; set; }
+        public string Street { get; set; }
+        public int Telephone { get; set; }
+
+        public string Valid(string FirstName)
+        {
+            if (FirstName != "")
+                //test to see if the first name has any characters
+                if (FirstName.Length < 21)
+                {
+                    return "";
+                }
+                else
+                {
+                    //return no error message
+                    return " The customer first name must not be more than 20 characters";
+                }
+            else
+            {
+                //return an error message
+                return "The customer first name cannot be left blank";
+            }
+        }
+    }
+}
