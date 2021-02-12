6ba95d5af562ddb1ada92f249f1e2af6f09611d8
Callum Simpson <callumjohnsimpson@hotmail.com>
2021-02-12 14:08:48 +0000

    Beginnings of Marketplace user collection

---
+++ VirginProject/VirginClassLibrary/clsMarketplaceUserCollection.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirginClassLibrary
+{
+    class clsMarketplaceUserCollection
+    {
+
+        //declare private variables for use list
+        List<clsMarketplaceUser> mUserList = new List<clsMarketplaceUser>();
+        //declarte private variables for a specific User
+        clsMarketplaceUser mThisUser = new clsMarketplaceUser();
+
+        //get set for User list
+        public List<clsMarketplaceUser> UserList
+        {
+
+            get
+            {
+                //return private data
+                return mUserList;
+            }
+            set
+            {
+                //set the private data
+                mUserList = value;
+            }
+        }
+
+    }
+}
