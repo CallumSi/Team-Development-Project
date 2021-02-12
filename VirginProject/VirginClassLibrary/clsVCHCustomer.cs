620c4de05688d7b270067f2a7b7e3290d7b774e1
EdwardTafi <59883163+EdwardTafi@users.noreply.github.com>
2021-02-12 18:17:13 +0000

    Redeveloped Tests

--- VirginProject/VirginClassLibrary/clsVCHCustomer.cs
+++ VirginProject/VirginClassLibrary/clsVCHCustomer.cs
@@ -4,42 +4,57 @@ namespace VirginClassLibrary
 {
     public class clsVCHCustomer
     {
-        //customer First Name
+        //customer FirstName property
         public string FirstName { get; set; }
 
-        //customer Last Name
+        //customer LastName property
         public string LastName { get; set; }
 
-        //customer Address
+        //customer Address property
         public string Address { get; set; }
 
-        //customer PhoneNumber
+        //customer PhoneNumber property
         public string PhoneNumber { get; set; }
 
-        //customer Email
+        //customer Email property
         public string Email { get; set; }
 
-        //customer Password
+        //customer Password property
         public string Password { get; set; }
 
-        public Boolean Valid(string FirstName)
+        public bool Valid(string FirstName, string LastName)
         {
-            //var to record any errors found in FirstName 
+            //var to record any errors foind in the FirstName (if everything is okay)
             Boolean OK = true;
-            //test to see if the FirstName has 1 character
+            
+            //test to see if the FirstName has one character
             if (FirstName.Length == 1)
             {
                 //set OK to false
                 OK = false;
             }
-            
-            //test to see that FirstName has no more than 30 character
+
+            //test to see the FirstName string has no more than 30 characters
             if (FirstName.Length > 30)
             {
                 //set OK to false
                 OK = false;
             }
 
+            //test to see if the LastName has one character
+            if (LastName.Length == 1)
+            {
+                //set OK to false
+                OK = false;
+            }
+
+            //test to see the LastName string has no more than 30 characters
+            if (LastName.Length > 30)
+            {
+                //set OK to false
+                OK = false;
+            }
+            
             //return all test results
             return OK;
         }
