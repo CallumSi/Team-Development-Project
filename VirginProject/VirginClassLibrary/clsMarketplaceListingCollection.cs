d7c571ee6fb65bdcfacdcde6357ac5c2aeb019ff
Callum Simpson <callumjohnsimpson@hotmail.com>
2021-02-13 17:10:59 +0000

    Added Add edit update delete and populate to marketplace user collection with find method ion marketplace user ready for database implementation.Also added report by email.

--- VirginProject/VirginClassLibrary/clsMarketplaceListingCollection.cs
+++ VirginProject/VirginClassLibrary/clsMarketplaceListingCollection.cs
@@ -3,11 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace VirginClassLibrary
 {
-    class clsMarketplaceListingCollection
+    public partial class clsMarketplaceListingCollection : clsMarketplaceListing
     {
-
+        
     }
 }
