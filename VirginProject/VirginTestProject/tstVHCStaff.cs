90770271c1556ef8d6c279599ab35d06efec8036
Mathunila Nitharsan <P17192949@my365.dmu.ac.uk>
2021-02-17 16:26:22 +0000

    Commented out staff test data

--- VirginProject/VirginTestProject/tstVHCStaff.cs
+++ VirginProject/VirginTestProject/tstVHCStaff.cs
@@ -15,14 +15,14 @@ namespace VirginTestProject
         //create some test data to pass to the method 
         //TOTAL: 9 Properties including PK (Staff_ID) 
 
-        string Staff_Title = "Dr"; // (1) 
-        string Staff_Firstname = "Monica"; // (2) 
-        string Staff_Lastname = "Geller"; // (3) 
-        string Staff_Email = "MGeller@gmail.com"; // (4) 
-        string Staff_Username = "MonGeller"; // (5) 
-        string Staff_Password = "MG3007"; // (6) 
-        string Staff_Role = "Doctor"; // (7) 
-        string Staff_Status = "True"; // (8)
+        //string Staff_Title = "Dr"; // (1) 
+        //string Staff_Firstname = "Monica"; // (2) 
+        //string Staff_Lastname = "Geller"; // (3) 
+        //string Staff_Email = "MGeller@gmail.com"; // (4) 
+        //string Staff_Username = "MonGeller"; // (5) 
+        //string Staff_Password = "MG3007"; // (6) 
+        //string Staff_Role = "Doctor"; // (7) 
+        //string Staff_Status = "True"; // (8)
 
         [TestMethod]
         public void InstantiationOK()
