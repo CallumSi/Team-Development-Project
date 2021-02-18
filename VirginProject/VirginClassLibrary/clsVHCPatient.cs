fc7b3f0b3a317de12b5ff8fe1ad0ee3bd649b210
Mathunila Nitharsan <P17192949@my365.dmu.ac.uk>
2021-02-18 16:26:00 +0000

    DOB validation need to check with Matthew

--- VirginProject/VirginClassLibrary/clsVHCPatient.cs
+++ VirginProject/VirginClassLibrary/clsVHCPatient.cs
@@ -113,26 +113,27 @@ namespace VirginClassLibrary
                 Error = Error + "PATIENT ADDRESS MUST BE BETWEEN 1 TO 100 CHARACTERS!" + " ";
             }
 
-            //(5) DOB Validation
-            try
-            {
-                //copy the patient DOB value to the DOBTemp variable
-                DOBTemp = Convert.ToDateTime(Patient_DOB);
+            ////(5) DOB Validation
+            //try
+            //{
+            //    //copy the patient DOB value to the DOBTemp variable
+            //    DOBTemp = Convert.ToDateTime(Patient_DOB);
 
-                //check to see if the DOB is not more than 31st December 2003
-                if (DOBTemp < DateTime.Now.Date.AddYears(18))
-                {
-                    //record the error
-                    //Error = Error + "YOU MUST BE OVER 18YRS!" + "  ";
-                }
+            //    //check to see if ...
+            //    if (DOBTemp < DateTime.Now.Date.AddYears(18))
+            //    {
+            //        //record the error
+            //        Error = Error + "YOU MUST BE OVER 18YRS!" + "  ";
 
-            }
+            //    }
 
-            catch
-            {
-                //record the error 
-                Error = Error + "THE PATIENT DATE OF BIRTH WAS NOT A VALID DATE : ";
-            }
+            //}
+
+            //catch
+            //{
+            //    //record the error 
+            //    Error = Error + "THE PATIENT DATE OF BIRTH WAS NOT A VALID DATE : ";
+            //}
 
             // (6) Patient Email Validation
 
