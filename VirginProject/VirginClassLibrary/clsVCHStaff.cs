e4d01d504d710734b961c066c9f5edbb18bf4fdd
EdwardTafi <59883163+EdwardTafi@users.noreply.github.com>
2021-02-18 14:12:05 +0000

    Further re-do

---
+++ VirginProject/VirginClassLibrary/clsVCHStaff.cs
@@ -0,0 +1,26 @@
+﻿namespace VirginClassLibrary
+{
+    public class clsVCHStaff
+    {
+        //staff first name property 
+        public string StaffFirstName { get; set; }
+        
+        //staff last name property 
+        public string StaffLastName { get; set; }
+
+        //staff address property 
+        public string StaffAddress { get; set; }
+
+        //staff postcode property 
+        public string StaffPostCode { get; set; }
+
+        //staff email property
+        public string StaffEmail { get; set; }
+
+        //staff password property
+        public string StaffPassword { get; set; }
+
+        //staff phone number property
+        public string StaffPhoneNumber { get; set; }
+    }
+}
