e35f8d7c8e2ddaa7857e6e6375939f747d3e993e
p17210836 <p17210836@GWHXX652WIN014W>
2021-02-27 18:05:17 +0000

    added tstVPDelivery, but some tests not working need to check this...

---
+++ VirginProject/VirginTestProject/tstVPOrder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace VirginTestProject
+{
+    [TestClass]
+    public class tstVPOrder
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
