fe73a05b9a3bf433d444a27be872e1bd6a10a964
Mathunila Nitharsan <P17192949@my365.dmu.ac.uk>
2021-03-29 23:49:32 +0100

    HospitalCollection: Testing Began - Add, Update, Delete and List completed coding in clsHospitalCollection and tstHospitalCollection (No Table Created)

--- VirginProject/VirginClassLibrary/clsVHCHospital.cs
+++ VirginProject/VirginClassLibrary/clsVHCHospital.cs
@@ -4,14 +4,87 @@ namespace VirginClassLibrary
 {
     public class clsVHCHospital
     {
+
+        //private data member for the Hospital property 
+        private int mHospital_ID;
+
+        //private data member for the Hospital_Name property 
+        private string mHospital_Name;
+
+        //private data member for the Hospital_Place property 
+        private string mHospital_Place;
+
         //Hospital_ID Property
-        public int Hospital_ID { get; set; }
+        public int Hospital_ID 
+        {
+            get
+            {
+                    //return the private data 
+                    return mHospital_ID;
+            }
+            set 
+            {
+                mHospital_ID = value; 
+            }
+        }
 
         //Hospital_Name Property
-        public string Hospital_Name { get; set; }
+        public string Hospital_Name
+
+        {
+            get
+            {
+                //return the private data 
+                return mHospital_Name;
+            }
+
+            set
+            {
+                mHospital_Name = value;
+            }
+        }
 
         //Hospital_Place Property
-        public string Hospital_Place { get; set; }
+        public string Hospital_Place
+        {
+            get
+            {
+                //return the private data 
+                return mHospital_Place;
+            }
+
+            set
+            {
+                mHospital_Place = value;
+            }
+        }
+
+        public bool Find(int Hospital_ID)
+        {
+            //create an instance of the data connection
+            clsDataConnection DB = new clsDataConnection();
+            //add the parameter for the Hospital_ID to search for
+            DB.AddParameter("@Hospital_ID", Hospital_ID);
+            //execute the stored procedure
+            DB.Execute("sproc_tblVHCHospital_FilterByVHCHospitalID");
+            //if one record is found (there should be either one or zero!)
+            if (DB.Count == 1)
+            {
+                //copy the data from the database to the private data members
+                mHospital_ID = Convert.ToInt32(DB.DataTable.Rows[0]["Hospital_ID"]);
+                mHospital_Name = Convert.ToString(DB.DataTable.Rows[0]["Hospital_Name"]);
+                mHospital_Place = Convert.ToString(DB.DataTable.Rows[0]["Hospital_Place"]);
+            
+                //return that everything worked OK
+                return true;
+            }
+            //if no record was found
+            else
+            {
+                //return false indicating a problem
+                return false;
+            }
+        }
 
         public string Valid(string Hospital_Name, string Hospital_Place)
         {
--- VirginProject/VirginTestProject/tstVHCHospitalCollection.cs
+++ VirginProject/VirginTestProject/tstVHCHospitalCollection.cs
@@ -1,14 +1,173 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VirginClassLibrary;
+
 
 namespace VirginTestProject
 {
     [TestClass]
     public class tstVHCHospitalCollection
     {
+      
+            [TestMethod]
+            public void InstantiationOK()
+            {
+                //create an instance of the hospital collection class 
+                clsVHCHospitalCollection AllHospitals = new clsVHCHospitalCollection();
+                //test to see that it exists 
+                Assert.IsNotNull(AllHospitals);
+            }
+
+            [TestMethod]
+            public void VHCHospitalListOK()
+            {
+            //create an instance of the hospital collection class 
+            clsVHCHospitalCollection AllHospitals = new clsVHCHospitalCollection();
+            //create a list of object to assign to the property
+            List<clsVHCHospital> TestList = new List<clsVHCHospital>();
+            //create the item of test data
+            clsVHCHospital TestItem = new clsVHCHospital();
+                //set its properties
+                TestItem.Hospital_ID = 1;
+                TestItem.Hospital_Name = "West Middlesex Hospital";
+                TestItem.Hospital_Place = "West London";
+                //add the item to the test list
+                TestList.Add(TestItem);
+                //assign the data to the property
+                AllHospitals.HospitalList = TestList;
+                //test to see that the two values are the same
+                Assert.AreEqual(AllHospitals.HospitalList, TestList);
+            }
+
         [TestMethod]
-        public void TestMethod1()
+        public void ThisHospitalPropertyOK()
         {
+            //create an instance of the hospital collection class 
+            clsVHCHospitalCollection AllHospitals = new clsVHCHospitalCollection();
+            //create some test data to assign to the property
+            clsVHCHospital TestHospital = new clsVHCHospital();
+            //set the properties of the test object
+            TestHospital.Hospital_ID = 1;
+            TestHospital.Hospital_Name = "West Middlesex Hospital";
+            TestHospital.Hospital_Place = "West London";
+            //assign the data to the property
+            AllHospitals.ThisHospital = TestHospital;
+            //test to see that the two values are the same
+            Assert.AreEqual(AllHospitals.ThisHospital, TestHospital);
         }
+
+        [TestMethod]
+        public void ListAndCountOK()
+        {
+            //create an instance of the hospital collection class 
+            clsVHCHospitalCollection AllHospitals = new clsVHCHospitalCollection();
+            //create some test data to assign to the property
+            //in this case the data needs to be a list of objects
+            List<clsVHCHospital> TestList = new List<clsVHCHospital>();
+            //add an item to the list
+            //create the item of test data
+            clsVHCHospital TestItem = new clsVHCHospital();
+            //set its properties
+            TestItem.Hospital_ID = 1;
+            TestItem.Hospital_Name = "West Middlesex Hospital";
+            TestItem.Hospital_Place = "West London";
+            //add the item to the test list
+            TestList.Add(TestItem);
+            //assign the data to the property
+            AllHospitals.HospitalList = TestList;
+            //test to see that the two values are the same
+            Assert.AreEqual(AllHospitals.Count, TestList.Count);
+        }
+
+        [TestMethod]
+        public void AddMethodOK()
+        //Add Method
+        {
+            //create an instance of the hospital collection class 
+            clsVHCHospitalCollection AllHospitals = new clsVHCHospitalCollection();
+            //create some test data to assign to the property
+            clsVHCHospital TestItem = new clsVHCHospital();
+            //var to store the primary key 
+            Int32 PrimaryKey = 0;
+            //set its properties 
+            TestItem.Hospital_ID = 1;
+            TestItem.Hospital_Name = "West Middlesex Hospital";
+            TestItem.Hospital_Place = "West London";
+            //set ThisHospital to the test data 
+            AllHospitals.ThisHospital = TestItem;
+            //add the record 
+            PrimaryKey = AllHospitals.Add();
+            //set the primary key of the test data 
+            TestItem.Hospital_ID = PrimaryKey;
+            //find the record 
+            AllHospitals.ThisHospital.Find(PrimaryKey);
+            //test to see that the two values are the same 
+            Assert.AreEqual(AllHospitals.ThisHospital, TestItem);
+        }
+
+        public void UpdateMethodOK()
+        //Add Method
+        {
+            //create an instance of the hospital collection class 
+            clsVHCHospitalCollection AllHospitals = new clsVHCHospitalCollection();
+            //create some test data to assign to the property
+            clsVHCHospital TestItem = new clsVHCHospital();
+            //var to store the primary key 
+            Int32 PrimaryKey = 0;
+            //set its properties 
+            TestItem.Hospital_ID = 1;
+            TestItem.Hospital_Name = "West Middlesex Hospital";
+            TestItem.Hospital_Place = "West London";
+            //set ThisPhone to the test data 
+            AllHospitals.ThisHospital = TestItem;
+            //add the record 
+            PrimaryKey = AllHospitals.Add();
+            //set the primary key of the test data 
+            TestItem.Hospital_ID = PrimaryKey;
+            //modify the test data 
+            TestItem.Hospital_ID = 1;
+            TestItem.Hospital_Name = "East Middlesex Hospital";
+            TestItem.Hospital_Place = "East London";
+            //set the record based on the new test data
+            AllHospitals.ThisHospital = TestItem;
+            //update the method 
+            AllHospitals.Update();
+            //find the record
+            AllHospitals.ThisHospital.Find(PrimaryKey);
+            //test to see ThisPhone matches the test data
+            Assert.AreEqual(AllHospitals.ThisHospital, TestItem);
+        }
+
+
+        public void DeleteMethodOK()
+        //Add Method
+        {
+            //create an instance of the hospital collection class 
+            clsVHCHospitalCollection AllHospitals = new clsVHCHospitalCollection();
+            //create some test data to assign to the property
+            clsVHCHospital TestItem = new clsVHCHospital();
+            //var to store the primary key 
+            Int32 PrimaryKey = 0;
+            //set its properties 
+            TestItem.Hospital_ID = 1;
+            TestItem.Hospital_Name = "West Middlesex Hospital";
+            TestItem.Hospital_Place = "West London";
+            //set ThisHospital to the test data 
+            AllHospitals.ThisHospital = TestItem;
+            //add the record 
+            PrimaryKey = AllHospitals.Add();
+            //set the primary key of the test data 
+            TestItem.Hospital_ID = PrimaryKey;
+            //find the record 
+            AllHospitals.ThisHospital.Find(PrimaryKey);
+            //delete the record 
+            AllHospitals.Delete();
+            //now find the record 
+            Boolean Found = AllHospitals.ThisHospital.Find(PrimaryKey);
+            //test to see that the two values are the same 
+            Assert.AreEqual(AllHospitals.ThisHospital, TestItem);
+        }
+
     }
 }
