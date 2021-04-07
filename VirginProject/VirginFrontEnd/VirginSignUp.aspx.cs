aa8363d394b6588bad30344ee74a2592761eeb30
Abdikarim Suldan <P2446237@my365.dmu.ac.uk>
2021-04-07 16:25:20 +0100

    added virgin home page and finished log in page

--- VirginProject/VirginFrontEnd/VirginSignUp.aspx.cs
+++ VirginProject/VirginFrontEnd/VirginSignUp.aspx.cs
@@ -9,29 +9,49 @@ namespace VirginFrontEnd
 {
     public partial class VirginSignUp : System.Web.UI.Page
     {
-        protected void Page_Load(object sender, EventArgs e)
+        private clsSecurity mSec;
+
+
+        public clsSecurity Sec
         {
-        
+            get
+            {
+                return mSec;
+            }
         }
 
-        protected void btnSignUp_Click(object sender, EventArgs e)
+        protected void Page_Load(object sender, EventArgs e)
         {
-            //create an instance of the security class
-            clsSecurity Sec = new clsSecurity();
-            //get the new users details
-            string Username = txtUsername.Text;
-            string Password = txtPassword.Text;
-            string PasswordConfirm = txtPasswordConfirm.Text;
-            string SecretMessage = txtSecret.Text;
+            mSec = new clsSecurity();
+        }
 
+      
 
-            //try to sign up the new user
-            lblError.Text = Sec.SignUp(Username, Password, PasswordConfirm, SecretMessage);
-            if (lblError.Text == "")
+        protected void btnLogIn_Click(object sender, EventArgs e)
+        {
+            //try to sign in and record any errors
+            bool Error = mSec.Login(txtUsername.Text, txtPassword.Text);
+            //if there were no errors
+            if (Error == true)
             {
-                Response.Redirect("VirginSignUpSuccess.aspx");
-
+                Response.Redirect("VirginHomePage.aspx");
             }
+            else
+
+            {
+                //otherwise show any errors
+                lblError.Text = "Failed Login";
+            }
+        }
+
+        protected void btnSignUp_Click1(object sender, EventArgs e)
+        {
+            Response.Redirect("VirginSignUp.aspx");
+        }
+
+        protected void btnForgotPassword_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("VirginForgotPassword.aspx");
         }
     }
 }
