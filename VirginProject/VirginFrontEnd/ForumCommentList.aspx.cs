0289373888d5f1cf1e49817bd2a33ce2696a49fc
Abdikarim Suldan <P2446237@my365.dmu.ac.uk>
2021-04-13 14:37:19 +0100

    added code to the comment list

--- VirginProject/VirginFrontEnd/ForumCommentList.aspx.cs
+++ VirginProject/VirginFrontEnd/ForumCommentList.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using VirginClassLibrary;
 
 namespace VirginFrontEnd
 {
@@ -11,7 +12,77 @@ namespace VirginFrontEnd
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            //if this is the first time the page is displayed
+            if (IsPostBack == false)
+            {
+                //update the list box
+                DisplayComments();
+            }
 
+        }
+        void DisplayComments()
+        {
+            //create an instance of the County Collection
+            clsForumCommentCollection Comment = new clsForumCommentCollection();
+            //set the data source to the list of countries in the collection
+            lstComment.DataSource = Comment.CommentList;
+            //set the name of the primary key
+            lstComment.DataValueField = "CommentID";
+            //set the data field to display
+            lstComment.DataTextField = "CommentMessage";
+            //bind the data to the list
+            lstComment.DataBind();
+        }
+
+        protected void btnEdit_Click(object sender, EventArgs e)
+        {
+            //var to store the primary key value of the record to be editied
+            Int32 CommentID;
+            //if a record has been selected from the list
+            if (lstComment.SelectedIndex != -1)
+            {
+                //get the primary key value of the record to edit
+                CommentID = Convert.ToInt32(lstComment.SelectedValue);
+                //store the data in the session object
+                Session["CommentID"] = CommentID;
+                //redirect to the edit page
+                Response.Redirect("ForumAnComment.aspx");
+            }
+            //if no recod has been selected
+            else
+            {
+                //display an error
+                lblError.Text = "Please make you select a comment from the list";
+
+            }
+        }
+
+        protected void btnDelete_Click(object sender, EventArgs e)
+        {
+            //var to store the primary key value of the record to be deleted
+            Int32 CommentID;
+            //if a record has been selected from the list
+            if (lstComment.SelectedIndex != -1)
+            {
+                //get the primary key value of the record to delete
+                CommentID = Convert.ToInt32(lstComment.SelectedValue);
+                //store the data in the session object
+                Session["CommentID"] = CommentID;
+                //redirect to the delete page
+                Response.Redirect("ForumCommentDelete.aspx");
+            }
+            //if no recod has been selected
+            else
+            {
+                //display an error
+                lblError.Text = "Please select a record to delete from the list";
+
+            }
+        }
+
+        protected void btnAdd_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("ForumPostList.aspx");
         }
     }
 }
