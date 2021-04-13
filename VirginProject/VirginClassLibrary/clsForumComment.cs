eb7e9eee62974d80c472765562da2ffa4ffbc8df
Abdikarim Suldan <P2446237@my365.dmu.ac.uk>
2021-04-13 14:06:11 +0100

    added my classes for comment

--- VirginProject/VirginClassLibrary/clsForumComment.cs
+++ VirginProject/VirginClassLibrary/clsForumComment.cs
@@ -1,9 +1,102 @@
-﻿namespace VirginClassLibrary
+﻿using System;
+
+namespace VirginClassLibrary
 {
     public class clsForumComment
     {
-        public clsForumComment()
+
+        //private data member for the PostID property
+        private Int32 mCommentID;
+        //private data member for the PostID property
+        private string mCommentMessage;
+        //private data member for the PostID property
+        private string mPostID;
+
+        public int CommentID
         {
+            get
+            {
+                //return the private data
+                return mCommentID;
+            }
+            set
+            {
+                //set the value of the private data member 
+                mCommentID = value;
+            }
         }
+        public string CommentMessage
+        {
+            get
+            {
+                //return the private data
+                return mCommentMessage;
+            }
+            set
+            {
+                //set the value of the private data member 
+                mCommentMessage = value;
+            }
+        }
+        public string PostID
+        {
+            get
+            {
+                //return the private data
+                return mPostID;
+            }
+            set
+            {
+                //set the value of the private data member 
+                mPostID = value;
+            }
+        }
+
+
+        public bool Find(int CommentID)
+        {
+            //create an instance of the data connection
+            clsDataConnection DB = new clsDataConnection();
+            //add the parameter for the CustomerID to search for
+            DB.AddParameter("@CommentID", CommentID);
+            //execute the stored procedure
+            DB.Execute("sproc_tblForumComment_FilterByCommentID");
+            //if one record is found (there should be either one or zero!)
+            if (DB.Count == 1)
+            {
+                //copy the data from the database to the private data member
+                mCommentID = Convert.ToInt32(DB.DataTable.Rows[0]["CommentID"]);
+                mCommentMessage = Convert.ToString(DB.DataTable.Rows[0]["CommentMessage"]);
+                //return that everything worked ok
+                return true;
+            }
+            //if no record was found
+            else
+            {
+                //return false inidating a problem
+                return false;
+            }
+        }
+
+        public string Valid(string CommentMessage)
+        {
+            //create a string varibale to store the error
+            String Error = "";
+            //if the PostTitle is blank
+            if (CommentMessage.Length == 0)
+            {
+                //record the error
+                Error = Error + "The Comment should not be blank.";
+            }
+            if (CommentMessage.Length > 300)
+            {
+                //record the error
+                Error = Error + "The comment can not be more that 300 characters.";
+            }
+            //return any error messages
+            return Error;
+        }
+
     }
+
 }
---
+++ VirginProject/VirginClassLibrary/clsForumCommentCollection.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections.Generic;
+using System;
+namespace VirginClassLibrary
+{
+    public class clsForumCommentCollection
+    {
+
+        //private data memeber for the list
+        List<clsForumComment> mCommentList = new List<clsForumComment>();
+        //private data menmber thisPost
+        clsForumComment mThisComment = new clsForumComment();
+        //public property for the comment list
+        public List<clsForumComment> CommentList
+        {
+            get
+            {
+                //return the private data
+                return mCommentList;
+            }
+            set
+            {
+                //set the priavte date
+                mCommentList = value;
+            }
+        }
+        public int Count
+        {
+            get
+            {
+                //return the count of the list
+                return mCommentList.Count;
+            }
+
+        }
+
+        public clsForumComment ThisComment
+        {
+            get
+            {
+                return mThisComment;
+            }
+            set
+            {
+                mThisComment = value;
+            }
+        }
+
+        public void Delete()
+        {
+            //delete the record pointed to by thisPost
+            //connect to the database
+            clsDataConnection DB = new clsDataConnection();
+            //set the parameters for the stored procedure
+            DB.AddParameter("@CommentID", mThisComment.CommentID);
+            //execute the stored procedure 
+            DB.Execute("sproc_tblForumComment_Delete");
+        }
+
+        //constructor for the class
+        public clsForumCommentCollection()
+        {
+            //object for data connection
+            clsDataConnection DB = new clsDataConnection();
+            //execute the store procedure
+            DB.Execute("sproc_tblForumComment_SelectAll");
+            //populate the array list with the data table
+            PopulateArray(DB);
+        }
+        void PopulateArray(clsDataConnection DB)
+        {
+
+            //populate the array list based on the data table in the parameter DB
+            //var for the index
+            Int32 Index = 0;
+            //var to store the record count
+            Int32 RecordCount;
+            //get the count record
+            RecordCount = DB.Count;
+            //clear the private array list
+            mCommentList = new List<clsForumComment>();
+            //while there are records to process
+            while (Index < RecordCount)
+            {
+                //create a blank post
+                clsForumComment AnComment = new clsForumComment();
+                //read in the fields from the current record
+                AnComment.CommentID = Convert.ToInt32(DB.DataTable.Rows[Index]["CommentID"]);
+                AnComment.CommentMessage = Convert.ToString(DB.DataTable.Rows[Index]["CommentMessage"]);
+                mCommentList.Add(AnComment);
+                //point at the next record
+                Index++;
+            }
+        }
+        public void AddComment(int PostID, string comment)
+        {
+            //reinitalise the data connection
+            clsDataConnection DB = new clsDataConnection();
+            //send the procedure to the parameters
+            DB.AddParameter("@PostID", PostID);
+            DB.AddParameter("@CommentMessage", comment);
+            //add the new record
+            DB.Execute("sproc_tblForumComment_Insert");
+        }
+
+        public void Update()
+        {
+            //update an existing record based on the values of thisPost
+            //connect to the database
+            clsDataConnection DB = new clsDataConnection();
+            //set the parameter for the stored procdure 
+            DB.AddParameter("@CommentID", mThisComment.CommentID);
+            DB.AddParameter("@CommentMessage", mThisComment.CommentMessage);
+            //execute the stored procedure
+            DB.Execute("sproc_tblForumComment_Update");
+        }
+    }
+}
