b0e6a07d2ded124224bf6a451fa4085c69c978f7
Abdikarim Suldan <P2446237@my365.dmu.ac.uk>
2021-04-15 09:46:45 +0100

    admin success page done

---
+++ VirginProject/VirginFrontEnd/ForumAdminSignUpSuccess.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace VirginFrontEnd
+{
+    public partial class ForumAdminSignUpSuccess : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnHome_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("ForumUserList.aspx");
+        }
+    }
+}
